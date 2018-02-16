6f642e4169d6b7d4e821961b4f1e713dc9e23ce6
bboampong <baffouraduboampong@gmail.com>
2018-02-16 20:04:41 +0000

    Fixed and Issue with App.vue after deleting Test.vue and users.vue

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,11 @@
 
 
 <script>
-import Test from './components/Test'
-import Users from './components/Users'
+
 
 export default {
   name: 'App',
   components: {
-    Test, Users
   }
 }
 </script>
